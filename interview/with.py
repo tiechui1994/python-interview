245b538c1f868c5d130b6e3c91d3eab2edaf97cb
tiechui1994 <2904951429@qq.com>
2018-06-06 23:12:20 +0800

    add interview content

---
+++ interview/with.py
@@ -0,0 +1,140 @@
+"""
+with相关概念:
+    上下文管理协议(Context Management Protocol): 包含方法__enter__()和__exit__(),支持该协议的对象要实现这两个方法.
+
+    上下文管理器(Context Manager): 支持上下文管理协议的对象，这种对象实现了__enter__()和__exit__()方法.上下文管理器定义执行with
+        语句时要建立的运行时上下文,负责执行with语句块上下文中的进入与退出操作.通常使用with语句调用上下文管理器,也可以通过直接调用其
+        方法来使用.
+    运行时上下文(runtime context)：由上下文管理器创建,通过上下文管理器的__enter__()和__exit__()方法实现,__enter__()方法在语句
+        体执行之前进入运行时上下文,__exit__()在语句体执行完后从运行时上下文退出.with语句支持运行时上下文这一概念.
+    上下文表达式(Context Expression)：with语句中跟在关键字with之后的表达式,该表达式要返回一个上下文管理器对象.
+    语句体(with-body)：with语句包裹起来的代码块,在执行语句体之前会调用上下文管理器的__enter__()方法,执行完语句体之后会执行
+        __exit__()方法.
+
+    with context_expression [as target(s)]:
+        with-body
+
+
+with语句执行流程:
+    context_manager = context_expression
+    exit = type(context_manager).__exit__
+    value = type(context_manager).__enter__(context_manager)
+    exc = True   # True表示正常执行,即便有异常也忽略; False表示重新抛出异常,需要对异常进行处理
+    try:
+        try:
+            target = value  # 如果使用了as子句
+            with-body     # 执行with-body
+        except:
+            # 执行过程中有异常发生
+            exc = exit(context_manager, *sys.exc_info())
+            # 如果__exit__返回True，则异常被忽略; 如果返回False,则重新抛出异常
+            # 由外层代码对异常进行处理
+            if not exec:
+                raise
+    finally:
+        # 正常退出; 或者通过statement-body中的break/continue/return语句退出; 或者忽略异常退出
+        if exc:
+            exit(context_manager, None, None, None)
+        # 缺省返回None, None在布尔上下文中看做是False
+
+
+    1. 执行context_expression, 生成上下文管理器context_manager
+    2. 调用上下文管理器的__enter__()方法; 如果使用了as子句,则将__enter__()方法的返回值赋值给as子句中的target(s)
+    3. 执行语句体with-body
+    4. 不管是否执行过程中是否发生了异常,执行上下文管理器的__exit__()方法,__exit__()方法负责执行"清理"工作,如释放资源等.
+       如果执行过程中没有出现异常,或者语句体中执行了语句break/continue/return,则以None作为参数调用__exit__(None, None, None);
+       如果执行过程中出现异常,则使用sys.exc_info得到的异常信息为参数调用__exit__(exc_type, exc_value, exc_traceback)
+    5. 出现异常时,如果__exit__(type, value, traceback)返回 False,则会重新抛出异常,让with之外的语句逻辑来处理异常,这也是通用做法;
+       如果返回 True,则忽略异常,不再对异常进行处理.
+"""
+
+"""
+自定义上下文管理器:(实现__enter__和__exit__)
+    context_manager.__enter__(): 进入上下文管理器的运行时上下文,在语句体执行前调用.with语句将该方法的返回值赋值给as子句中的target,
+        如果指定了as子句的话
+    context_manager.__exit__(exc_type, exc_value, exc_traceback): 退出与上下文管理器相关的运行时上下文,返回一个布尔值表示是否
+        对发生的异常进行处理. 参数表示引起退出操作的异常.
+            如果退出时没有发生异常,则3个参数都为None,同时返回True.
+            如果发生异常,返回True表示不处理异常,否则会在退出该方法后重新抛出异常以由with语句之外的代码逻辑进行处理.
+            如果该方法内部产生异常,则会取代由statement-body中语句产生的异常.(最终抛出的异常是该方法内部抛出的异常)
+"""
+
+
+class ContextProtocol(object):
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __enter__(self):
+        print('__enter__')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('__exit__')
+        if exc_tb is None:
+            print('normal')
+            return True
+        else:
+            raise Exception('exit inner exception')
+
+
+try:
+    with ContextProtocol('tag') as f:
+        print('exec body')
+        raise Exception('body exception')
+except Exception as e:
+    print(e)  # exit inner exception
+
+"""
+contextlib 模块:
+    contextlib模块提供了3个对象: 装饰器contextmanager, 函数nested和上下文管理器closing. 用这些对象,可以对已有的生成器函数
+    或者对象进行包装,加入对上下文管理协议的支持,避免了专门编写上下文管理器来支持with语句
+
+    contextmanager: 用于对生成器函数进行装饰,生成器函数被装饰以后,返回的是一个上下文管理器,其__enter__()和__exit__()方法由
+        contextmanager负责提供. 被装饰的生成器函数只能产生一个值,否则会导致异常RuntimeError;产生的值会赋值给as子句中的target
+
+
+    closing: 上下文自动关闭块末尾的内容. closing 适用于提供了 close() 实现的对象
+        with closing(<module>.open(<arguments>)) as f:
+            <block>
+
+        实现:
+            def __init__(self, thing):
+                self.thing = thing
+            def __enter__(self):
+                return self.thing
+            def __exit__(self, *exc_info): // 亮点
+                self.thing.close()
+
+    ContextDecorator: 上下文管理器 + 装饰器 (继承该类其实例既可以作为上下文管理器对象,又可以作为装饰器)
+
+    class context(ContextDecorator):
+        def __enter__(self):
+          return self
+
+       def __exit__(self, *exc):
+          return False
+
+    @context()
+    def say():
+        print('hello')
+
+    say()可以按照上下文管理器的方式运行
+
+    等价于=>  with context():
+                print('hello')
+"""
+
+from contextlib import contextmanager, ContextDecorator
+
+
+@contextmanager
+def demo():
+    print('Code before yield-statement executes in __enter__')
+    # 生成器函数的只能产生一个值
+    for i in range(0, 1):
+        yield '*** contextmanager %d ***' % i
+    print('Code after yield-statement executes in __exit__')
+
+
+with demo() as value:
+    print(value)
---
+++ threadModule/data_share_and_sync.py
@@ -0,0 +1,111 @@
+"""
+array模块:
+    定义了一个新对象类型array.该类型工作原理与列表及其相似, 但是它的内容仅限于单一类型. 数组的类型在创建数组时确定.
+        类型编码    描述          C类型         最小大小(字节)
+        'c'        8位字符        Char              1
+        'b'        8位整形        signed char       1
+        'B'        8为无符号整形   unsigned char    1
+        'i'        整形           int              4或8
+        'I'        无符号整形      unsigned int    4或8
+        'l'        长整形          long            4或8
+        'L'        无符号长整形    unsigned long    4或8
+        'h'        16位整形        short           2
+        'H'        16位无符号整形  unsigned short   2
+        'f'        单精度浮点型    float            4
+        'd'        双精度浮点型    double           8
+        'u'        Unicode字符                     2或4
+
+        array(typecode, initializer)
+        方法:
+            a.itemsize  存储在数组中项的大小(字节为单位)
+            append(x)
+            buffer_info()  返回(addres, length) 提供用于存储数组的缓存区的内存位置和长度
+            byteswap()   在大尾与小尾之间切换数组中所有项的字节顺序,仅支持整形值
+            count(x)
+            extend(b)    数组合并
+            fromfile(f, n) 从文件对象f中读取n个项(二进制格式),并追加到数组末尾. f必须是一个文件对象,且可读取的项大于n
+            fromlist(list) 追加list
+            fromstring(s) 追加字符串s中的项,s是一个二进制组成的字符串, 与fromfile()读取方式相同
+            index(x)   索引
+            insert(i, x)
+            pop([i])
+            remove(x)
+            reverse()
+            tofile(f)  写入文件f,数据保存为二进制格式
+            tolist()
+            tostring()
+            tounicode()   数组的类型必须是'u'
+
+
+
+数据共享与同步: 通常,进程之间彼此是完全孤立的,唯一的通信方式是队列或管道. 但可以使用两个对象来表示共享数据.
+              其实, 这些对象使用了共享内存(通过mmap模块)使访问多个进程成为可能.
+
+    Value(typecode, arg1, arg2, ... argN, lock)  在共享内存中创建ctypes对象. typecode要么包含array模块使用的相同类型代码(如
+        'i', 'd'等)的字符串,要么是来自ctypes模块的类型对象(如ctypes.c_int, ctypes.c_double等). 所有额外的位置参数arg1, arg2,
+        将传递给指定类型的构造函数. lock是只能使用关键字调用的参数,如果是True(默认值),将创建一个新锁来保护对值的访问. 如果传入一个
+        现有锁,如Lock或RLock实例,该锁将用于进行同步. 如果v是Value创建的共享值的实例,便可以使用v.value访问底层的值. eg: x=v.value
+        v.value=xx
+
+    Array(typecode, initializer [, lock]) 在共享内存当中创建ctypes数组. typecode描述了数组的内容,意义与Value当中的相同.
+        initializer要么是设置数组初始大小的整数,要么是项序列,其值和大小用于初始化数组. lock同Value当中的含义. 可以使用标准的
+        Python索引,切片,迭代操作访问其内容,每种操作均由锁进行同步. 对应字节字符串,具备a.value访问.
+
+    其他同步原语:
+        Lock 互斥锁
+        RLock 可重入的互斥锁(同一进程可以多次获得它,同时不会造成阻塞)
+        Semaphore 信号量
+        BoundSemaphore 有边界的信号量
+        Event 事件
+        Condition 条件变量
+    使用与threading类似.
+
+    注意: 使用多进程后,不需要担心与锁,信号量或类似构造的底层同步. 在某种程度上,管道的send()和receive(),以及队列的put()
+          和get()已经提供了同步功能. 但是,在使用数据共享(Value, Array)的时候, 需要使用锁,信号量等机制进行同步操作.
+"""
+
+import multiprocessing
+
+
+class FloatChannel(object):
+    def __init__(self, maxsize):
+        self.buffer = multiprocessing.Array('f', maxsize, lock=False)
+        self.buffer_len = multiprocessing.Value('i')
+        self.empty = multiprocessing.Semaphore(1)
+        self.full = multiprocessing.Semaphore(0)
+
+    def send(self, values):
+        self.empty.acquire()       # 只有在缓存区为空的时候才继续
+        values_length = len(values)
+        self.buffer_len = values_length
+        self.buffer[:values_length] = values
+        self.full.release()        # 发送信号通知缓存区已满
+
+    def receive(self):
+        self.full.acquire()      # 只在缓存区已满的时候继续
+        values = self.buffer[:self.buffer_len.value]
+        self.empty.release()     # 发送信号通知缓存区已空
+        return values
+
+
+def consume(count, ch):
+    for i in range(count):
+        values = ch.receive()
+        print('receive ', values)
+
+
+def produce(count, values, ch):
+    for i in range(count):
+        ch.send(values)
+        print('send ', values)
+
+
+if __name__ == '__main__':
+    ch = FloatChannel(100)
+    p = multiprocessing.Process(target=consume, args=(50, ch))
+
+    p.start()
+    values = [float(x) for x in range(100)]
+    produce(10, values, ch)
+    print('Done')
+    p.join()
